ca020203fed269668ab0903e7ae458d49111586d
Christian Chaux <xwilarg@protonmail.com>
2023-05-01 15:36:31 +0200

    Add CMake file

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_opt LPSTR lpCmdLine, __in int nShowCmd)
+{
+    MessageBox(NULL, "Hello, World!", "And the world never replied...", 0x00000000L);
+}
