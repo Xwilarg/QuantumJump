27427e20586a57f10ad3f9c4a0853bd1e389ba5c
Christian Chaux <xwilarg@protonmail.com>
2023-05-07 19:15:43 +0200

    Fix object not appearing

--- src/game.c
+++ src/game.c
@@ -17,23 +17,22 @@ void GAME_USERInit(Game* g, Context* ctx)
 
 		Renderer* r = RENDERER_New("demo.mesh", "demo.tex");
 		Rigidbody* rb = RIGIDBODY_New();
-		//RIGIDBODY_AddForce(rb, VECTOR_New(0.f, 25.f, 0.f));
+		RIGIDBODY_AddForce(rb, VECTOR_New(0.f, 25.f, 0.f));
 
 		OBJECT_AddComponent(dynObj, r->parent);
-		OBJECT_AddComponent(dynObj, rb->parent);
+		OBJECT_AddComponent(dynObj, rb->parent);;
+
+		GAME_AddObject(g, dynObj);
 	}
 
 	{
 		Object* ref = OBJECT_New();
 		ref->transform->position.z = -200.f;
-		ref->transform->position.y = -50.f;
+		ref->transform->position.y = -70.f;
 
 		Renderer* r = RENDERER_New("demo.mesh", "demo.tex");
-		Rigidbody* rb = RIGIDBODY_New();
-		//RIGIDBODY_AddForce(rb, VECTOR_New(0.f, 25.f, 0.f));
 
 		OBJECT_AddComponent(ref, r->parent);
-		OBJECT_AddComponent(ref, rb->parent);
 
 		GAME_AddObject(g, ref);
 	}
