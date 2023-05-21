ac314339df579698169d438fe6f0e77d0791e557
Xwilarg <xwilarg@yahoo.fr>
2023-05-21 22:14:17 +0200

    Improve jumps

--- inc/config.h
+++ inc/config.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define CONFIG_SPEED 7500.f
-#define CONFIG_JUMP_FORCE 100.f
-#define CONFIG_GRAVITY -100.f
+#define CONFIG_SPEED 13000.f
+#define CONFIG_JUMP_FORCE 150.f
+#define CONFIG_GRAVITY -300.f
