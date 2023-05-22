83cb37627dea3edd35f1b82ce827ea5dc3439e28
Xwilarg <xwilarg@yahoo.fr>
2023-05-22 15:14:28 +0200

    Improve gravity management

--- src/component/rigidbody.c
+++ src/component/rigidbody.c
@@ -15,7 +15,13 @@ static void Update(Object* o, Game* game, Context* ctx, void* self)
 	rb->linearVelocity = VECTOR_Multiply(rb->linearVelocity, 1.f - rb->linearDrag * ctx->time->deltaTime);
 	if (rb->useGravity)
 	{
+		float prevY = rb->linearVelocity.y;
+		float minY = min(prevY, CONFIG_Y_TERMINAL_VELOCITY); // Allow override of terminal velocity if input manually
 		rb->linearVelocity = VECTOR_Add(rb->linearVelocity, VECTOR_New(0.f, CONFIG_GRAVITY * ctx->time->deltaTime, 0.f));
+		if (rb->linearVelocity.y < minY)
+		{
+			rb->linearVelocity.y = minY;
+		}
 	}
 	rb->angularVelocity = VECTOR_Multiply(rb->angularVelocity, 1.f - rb->angularDrag * ctx->time->deltaTime);
 
